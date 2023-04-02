3854c315f7cbfa096f77deede647618dd83820df
ZhengyuanHan <129567395+ZhengyuanHan@users.noreply.github.com>
2023-04-02 10:40:28 -0400

    Add files via upload

--- adventure.py
+++ adventure.py
@@ -32,7 +32,7 @@ def print_room_info(cur_room):
     print(exit_str + '\n')
 
 
-# py adventure.py world.map
+# py adventure.py asset.map
 # py adventure.py loop.map
 if __name__ == '__main__':
     map_name = sys.argv[1]
@@ -65,9 +65,18 @@ if __name__ == '__main__':
                             print('The direction is locked, you should find the key first.')
                             print('What would you like to do?', end=" ")
                         else:
-                            if whole_map[room_id]["name"] == "internet cafe":
-                                print(whole_map[room_id]['fail_dialog'])
-                                break
+
+                            if whole_map[room_id]["name"] == "system room":
+                                if 'h size battery' not in inventory_list:
+                                    if 'm size battery' in inventory_list:
+                                        print(whole_map[room_id]['lose_desc2'])
+                                        break
+                                    else:
+                                        print(whole_map[room_id]['lose_desc1'])
+                                        break
+                                else:
+                                    print(whole_map[room_id]['win_desc'])
+                                    break
 
                             print('You go ' + direction + '.' + '\n')
                             cur_room = whole_map[room_id]
@@ -119,39 +128,10 @@ if __name__ == '__main__':
                     print("There's no " + item_name + " anywhere.")
                     print('What would you like to do?', end=" ")
 
-            elif re.match(r'\s*drop\s*$', verb, re.I):
-                print("Sorry, you need to 'drop' something.")
-                print('What would you like to do?', end=" ")
-
-            elif re.match(r'\s*drop\s*.+\s*$', verb, re.I):
-                hold_name = re.search(r'(?<=drop\s).+', verb, re.I).group().lower()
-                if hold_name in inventory_list:
-                    if 'items' not in cur_room:
-                        cur_room.update({'items': []})
-
-                    cur_room['items'].append(hold_name)
-                    if cur_room['name'] == 'living room' and re.match(r'screen', hold_name):
-                        cur_room['screen_fragments'].append(hold_name)
-                    inventory_list.remove(hold_name)
-                    print('You drop the ' + hold_name + '.')
-
-                    if cur_room['name'] == "branch road" or cur_room['name'] == "mini mart":
-                        cur_room['environment_desc'] = cur_room['environment_desc_backup'][1]
-                        print(cur_room['environment_desc'])
-
-                    if cur_room['name'] == 'living room' and len(cur_room['screen_fragments']) == 5:
-                        print(cur_room['special_desc'])
-                        cur_room = whole_map[12]
-                        print_room_info(cur_room)
-
-                    print('What would you like to do?', end=" ")
-                else:
-                    print("You are not hanging " + hold_name + ".")
-                    print('What would you like to do?', end=" ")
-
             elif re.match(r'\s*talk\s*$', verb, re.I):
                 if 'npc' in cur_room:
                     print(cur_room['npc']['name'] + ': ' + cur_room['npc']['dialog'])
+                    print('What would you like to do?', end=" ")
                 else:
                     print("Nobody around here.")
                     print('What would you like to do?', end=" ")
@@ -163,20 +143,21 @@ if __name__ == '__main__':
             elif re.match(r'\s*give\s*.+\s*$', verb, re.I):
                 gift_name = re.search(r'(?<=give\s).+', verb, re.I).group().lower()
                 if gift_name in inventory_list:
-                    if cur_room['npc']['require'] != gift_name:
+                    if 'require' not in cur_room['npc']:
+                        print("This guy doesn't need anything.")
+                        print('What would you like to do?', end=" ")
+                    elif cur_room['npc']['require'] != gift_name:
                         print("This guy doesn't need that.")
+                        print('What would you like to do?', end=" ")
                     else:
                         inventory_list.remove(gift_name)
                         print('You give the ' + gift_name + '.')
                         print(cur_room['npc']['name'] + ': ' + cur_room['npc']['answer'] + '\n')
+                        cur_room['npc']['dialog'] = cur_room['npc_dialog_after_giving']
+                        inventory_list.append(cur_room['npc']['give_back'])
+                        print('You get the ' + cur_room['npc']['give_back'] + '.')
+                        print('What would you like to do?', end=" ")
 
-                        if cur_room['npc']['name'] == "driver":
-                            print(cur_room['win_dialog'])
-                            break
-                        else:
-                            cur_room['npc']['dialog'] = cur_room['npc_dialog_after_giving']
-                            inventory_list.append(cur_room['npc']['give_back'])
-                            print('You get the ' + cur_room['npc']['give_back'] + '.')
                 else:
                     print("You are not hanging " + gift_name + ".")
                     print('What would you like to do?', end=" ")
@@ -185,25 +166,8 @@ if __name__ == '__main__':
                 print("Goodbye!")
                 break
 
-            elif re.match(r'\s*help\s*$', verb, re.I):
-                print("One day you are doing an experiment in your home, suddenly, an accident happens. You are "
-                      "transported to a strange place by your transport machine. You don't bring your phone, only one "
-                      "check in your pocket, and the time is very late. The aim of this game is to find a way to go back home." + '\n')
-
-                print("You can run the following commands:")
-                print("go...")
-                print("get...")
-                print("look")
-                print("inventory")
-                print("drop...")
-                print("talk")
-                print("give...")
-                print("quit")
-                print("help")
-                print('What would you like to do?', end=" ")
-
             else:
-                print("Invalid command, please check your spell, or enter 'help' to get more information.")
+                print("Invalid command, please check your spell.")
                 print('What would you like to do?', end=" ")
 
         except EOFError:
